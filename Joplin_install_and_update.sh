3173971afa8709548014c8c36a211b03a5afddcc
pavelgendelman <pavelgendelman@coherentsolutions.com>
2020-02-19 17:46:57 +0300

    additional install request for ubuntu

--- Joplin_install_and_update.sh
+++ Joplin_install_and_update.sh
@@ -109,6 +109,11 @@ if [[ ! -e ~/.joplin/VERSION ]] || [[ $(< ~/.joplin/VERSION) != "$RELEASE_VERSIO
        mkdir -p ~/.local/share/applications
        echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=Joplin\nComment=Joplin for Desktop\nExec=/home/$USER/.joplin/Joplin.AppImage\nIcon=joplin\nStartupWMClass=Joplin\nType=Application\nCategories=Office;\n#$APPIMAGE_VERSION" >> ~/.local/share/applications/appimagekit-joplin.desktop
        # Update application icons
+        if [[ $DESKTOP =~ .*unity.* ]]
+        then
+          echo 'Permisson to create desktop file'
+          [[ `command -v desktop-file-install` ]] && sudo desktop-file-install ~/.local/share/applications/appimagekit-joplin.desktop
+        fi
        [[ `command -v update-desktop-database` ]] && update-desktop-database ~/.local/share/applications
        print "${COLOR_GREEN}OK${COLOR_RESET}"
     else
